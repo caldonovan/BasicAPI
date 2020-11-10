85a3a05256f7cfbfd6e97ece115d38f155cafceb
Cal Donovan <caldonovan@outlook.com>
2020-11-10 17:36:59 +0000

    Added the remaining routes for each model.

--- routes/api.php
+++ routes/api.php
@@ -24,11 +24,20 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 // Get all the companies in the database and return JSON.
 Route::get('/companies', [CompanyController::class, 'index']);
 Route::get('/companies/{company}', [CompanyController::class, 'show']);
+Route::post('/companies', [CompanyController::class, 'store']);
+Route::put('/company/{company}', [CompanyController::class, 'update']);
+Route::delete('/company/{company}', [CompanyController::class, 'delete']);
 
 // Get all the users in the database and return JSON
 Route::get('/users', [UserController::class, 'index']);
 Route::get('/users/{user}', [UserController::class, 'show']);
+Route::post('/users', [UserController::class, 'store']);
+Route::put('/users/{user}', [UserController::class, 'update']);
+Route::delete('/users/{user}', [UserController::class, 'delete']);
 
 // Get all the servers in the database and return JSON
 Route::get('/servers', [ServerController::class, 'index']);
 Route::get('/servers/{server}', [ServerController::class, 'show']);
+Route::post('/servers', [ServerController::class, 'store']);
+Route::put('/servers/{server}', [ServerController::class, 'update']);
+Route::delete('/servers/{server}', [ServerController::class, 'delete']);
