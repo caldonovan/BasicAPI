393503f7d14ae0dfb75dfb9b24ba7eb6ee0c59cd
Cal Donovan <caldonovan@outlook.com>
2020-11-13 16:07:49 +0000

    Added all REST methods to CompanyController.

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -16,4 +16,46 @@ class CompanyController extends Controller
             return "Encountered an error: " . $e->getMessage();
         }
     }
+
+    public function show(Company $company)
+    {
+        try {
+            return Company::find($company->id);
+        } catch (Exception $e) {
+            return "Encountered an error: " . $e->getMessage();
+        }
+    }
+
+    public function store(Request $request)
+    {
+        try {
+            Company::create($request->all());
+
+            return response()->json('New company created!', 200);
+        } catch (Exception $e) {
+            return "Encountered an error: " . $e->getMessage();
+        }
+    }
+
+    public function update(Request $request, Company $company)
+    {
+        try {
+            $company->update($request->all());
+
+            return response()->json($company, 200);
+        } catch (Exception $e) {
+            return "Encountered an error: " . $e->getMessage();
+        }
+    }
+
+    public function delete(Company $company)
+    {
+        try {
+            $company->delete();
+
+            return response()->json('Company deleted', 204);
+        } catch (Exception $e) {
+            return "Encountered an error: " . $e->getMessage();
+        }
+    }
 }
