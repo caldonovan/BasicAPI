6d3fa05c4853bb2c3b1a0078827023d10e79f8e2
Cal Donovan <caldonovan@outlook.com>
2020-11-16 19:01:47 +0000

    Updated dependencies.

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
 Route::get('/v', function () {
     return 'Laravel ' . app()->version();
@@ -25,6 +25,10 @@ Route::get('/v', function () {
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/**
+ * As this isn't a secure API, we don't need to use the auth middleware.
+ */
 
 // Get all the companies in the database and return JSON.
 Route::get('/companies', [CompanyController::class, 'index']);
