ae31bef7184559419662b2579303897f0a537fde
Cal Donovan <caldonovan@outlook.com>
2020-11-17 15:03:35 +0000

    Changed api route comments to be more concise.

--- routes/api.php
+++ routes/api.php
@@ -27,31 +27,35 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 /**
- * As this isn't a secure API, we don't need to use the auth middleware.
+ * Companies
  */
-
-// Get all the companies in the database and return JSON.
 Route::get('/companies', [CompanyController::class, 'index']);
 Route::get('/companies/{company}', [CompanyController::class, 'show']);
 Route::post('/companies', [CompanyController::class, 'store']);
 Route::put('/companies/{company}', [CompanyController::class, 'update']);
 Route::delete('/companies/{company}', [CompanyController::class, 'delete']);
 
-// Get all the users in the database and return JSON
+/**
+ * Users
+ */
 Route::get('/users', [UserController::class, 'index']);
 Route::get('/users/{user}', [UserController::class, 'show']);
 Route::post('/users', [UserController::class, 'store']);
 Route::put('/users/{user}', [UserController::class, 'update']);
 Route::delete('/users/{user}', [UserController::class, 'delete']);
 
-// Get all the servers in the database and return JSON
+/**
+ * Servers
+ */
 Route::get('/servers', [ServerController::class, 'index']);
 Route::get('/servers/{server}', [ServerController::class, 'show']);
 Route::post('/servers', [ServerController::class, 'store']);
 Route::put('/servers/{server}', [ServerController::class, 'update']);
 Route::delete('/servers/{server}', [ServerController::class, 'delete']);
 
-// Get all the adverts and return JSON
+/**
+ * Adverts
+ */
 Route::get('/adverts', [AdvertController::class, 'index']);
 Route::get('/adverts/{advert}', [AdvertController::class, 'show']);
 Route::post('/adverts', [AdvertController::class, 'store']);
