da52368d6ded963f5197f3fbe1fbefece415df98
Cal Donovan <caldonovan@outlook.com>
2020-11-18 18:25:16 +0000

    Try / Catch not needed since custom error handler implemented.

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -3,18 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\Company;
-use Exception;
 use Illuminate\Http\Request;
 
 class CompanyController extends Controller
 {
     public function index()
     {
-        try {
-            return Company::all();
-        } catch (Exception $e) {
-            return response()->json('Error!: ' . $e->getMessage(), 404);
-        }
+        return Company::all();
     }
 
     public function show(Company $company)
@@ -24,34 +19,30 @@ class CompanyController extends Controller
 
     public function store(Request $request)
     {
-        try {
-            Company::create($request->all());
+        $company = Company::create($request->all());
 
-            return response()->json('New company created!', 200);
-        } catch (Exception $e) {
-            return "Encountered an error: " . $e->getMessage();
-        }
+        return response()->json([
+            "status" => "success",
+            "message" => "Successfully created new Company!",
+            "details" => $company,
+        ], 200);
     }
 
     public function update(Request $request, Company $company)
     {
-        try {
-            $company->update($request->all());
+        $company->update($request->all());
 
-            return response()->json($company, 200);
-        } catch (Exception $e) {
-            return "Encountered an error: " . $e->getMessage();
-        }
+        return response()->json([
+            "status" => "success",
+            "message" => "Successfully updated Company with ID: " . $company->id,
+            "details" => $company,
+        ], 200);
     }
 
     public function delete(Company $company)
     {
-        try {
-            $company->delete();
+        $company->delete();
 
-            return response()->json('Company deleted', 204);
-        } catch (Exception $e) {
-            return "Encountered an error: " . $e->getMessage();
-        }
+        return response()->json('Company deleted', 204);
     }
 }
