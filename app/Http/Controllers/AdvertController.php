902bdc577d8029f4f9fb6ffea3e2c429da1a0b62
Cal Donovan <caldonovan@outlook.com>
2020-11-27 12:39:04 +0000

    🚧 Remove 'try...catch' from Advert Controller.

--- app/Http/Controllers/AdvertController.php
+++ app/Http/Controllers/AdvertController.php
@@ -3,63 +3,38 @@
 namespace App\Http\Controllers;
 
 use App\Models\Advert;
-use Exception;
 use Illuminate\Http\Request;
 
 class AdvertController extends Controller
 {
     public function index()
     {
-        try {
-            return Advert::all();
-        } catch (Exception $e) {
-            return 'We\'ve caught an error!: ' . $e->getMessage();
-        }
+        return Advert::all();
     }
 
     public function show(Advert $advert)
     {
-        try
-        {
-            return Advert::find($advert->id);
-        } catch (Exception $e) {
-            return 'We\'ve caught an error!: ' . $e->getMessage();
-        }
+        return Advert::find($advert->id);
     }
 
     public function store(Request $request)
     {
-        try
-        {
-            $advert = Advert::create($request->all());
+        $advert = Advert::create($request->all());
 
-            return response()->json($advert, 201);
-
-        } catch (Exception $e) {
-            return 'We\'ve caught an error!: ' . $e->getMessage();
-        }
+        return response()->json($advert, 201);
     }
 
     public function update(Request $request, Advert $advert)
     {
-        try {
-            $advert->update($request->all());
+        $advert->update($request->all());
 
-            return response()->json($advert->id . ' updated!', 200);
-        } catch (Exception $e) {
-            return 'We\'ve caught an error!: ' . $e->getMessage();
-        }
+        return response()->json($advert->id . ' updated!', 200);
     }
 
     public function delete(Advert $advert)
     {
-        try
-        {
-            $advert->delete();
+        $advert->delete();
 
-            return response()->json($advert->id . ' deleted!', 204);
-        } catch (Exception $e) {
-            return 'We\'ve caught an error!: ' . $e->getMessage();
-        }
+        return response()->json($advert->id . ' deleted!', 204);
     }
 }
