717b76f088ca54e0cf3f23192bf03fbb1dab399a
Cal Donovan <caldonovan@outlook.com>
2020-12-03 23:46:30 +0000

    📦 Installed 'laravel/breeze'.

--- app/Http/Controllers/AdvertController.php
+++ app/Http/Controllers/AdvertController.php
@@ -19,9 +19,23 @@ class AdvertController extends Controller
 
     public function store(Request $request)
     {
-        // $request->all() SHOULD NOT BE USED IN A PROD ENV.
-        // Realistically if implemented comercially, the
-        // request should be fully validated.
+        $this->validate($request, [
+            'title' => 'required|string|max:100',
+            'subtitle' => 'string|max:72',
+            'description' => 'required|string|max:172',
+            'registration' => 'requred|string|max:8',
+            'make' => 'required|string|max:24',
+            'model' => 'required|string|max:48',
+            'trim' => 'string|max:48',
+            'price' => 'required|string|max:6',
+            'mileage' => 'required|max:16',
+            'engine_size' => 'string|max:16',
+            'doors' => 'string|max:1',
+            'body_style' => 'string|max:16',
+            'transmission' => 'required|string|max:24',
+            'fuel_type' => 'required|string|max:8',
+        ]);
+
         $advert = Advert::create($request->all());
 
         return response()->json($advert, 201);
