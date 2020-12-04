490b776131a1a70e572b5dd324153693e36caa31
Cal Donovan <caldonovan@outlook.com>
2020-12-04 00:10:58 +0000

    🚧 Added validation to CompanyController

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -14,35 +14,39 @@ class CompanyController extends Controller
 
     public function show(Company $company)
     {
-        return Company::findOrFail($company->id);
+        return Company::find($company->id);
     }
 
     public function store(Request $request)
     {
+        $this->validate($request, [
+            'company_name' => 'required|string|max:72',
+            'company_address' => 'required|string|max:150',
+            'company_motto' => 'string|max:72'
+        ]);
+
         $company = Company::create($request->all());
 
-        return response()->json([
-            "status" => "success",
-            "message" => "Successfully created new Company!",
-            "details" => $company,
-        ], 200);
+        return response()->json('Company ' . $company->company_name . ' created!', 201);
     }
 
     public function update(Request $request, Company $company)
     {
+        $this->validate($request, [
+            'company_name' => 'required|string|max:72',
+            'company_address' => 'required|string|max:150',
+            'company_motto' => 'string|max:72'
+        ]);
+
         $company->update($request->all());
 
-        return response()->json([
-            "status" => "success",
-            "message" => "Successfully updated Company with ID: " . $company->id,
-            "details" => $company,
-        ], 200);
+        return response()->json('Company ' . $company->company_name . ' updated!', 201);
     }
 
     public function delete(Company $company)
     {
         $company->delete();
 
-        return response()->json('Company deleted', 204);
+        return response()->json('Company ' . $company->id . ' deleted', 204);
     }
 }
